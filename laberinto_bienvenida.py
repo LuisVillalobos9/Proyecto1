9f4ba8137d196eb5f0c1cc3f375ec89cae4dc8e0
LuisVillalobos9 <149641240+LuisVillalobos9@users.noreply.github.com>
2023-12-06 20:35:36 -0500

    Add files via upload

---
+++ laberinto_bienvenida.py
@@ -0,0 +1,23 @@
+# Beta del laberinto
+laberinto = [
+    ['P', '.', '#', '#', '#', '#',],
+    ['#', '.', '.', '.', '#', '.',],
+    ['#', '#', '#', '.', '#', '.',],
+    ['#', '.', '.', '.', '.', '.',],
+    ['#', '.', '#', '#', '#', '#',],
+    ['#', '.', '.', '.', '#', '#',],
+    ['#', '.', '#', '.', '.', '.',],
+    ['#', '#', '#', '#', '#', '.',]
+]
+
+#Nombre del laberinto
+laberinto_nombre = "CraZy MaZe RuNNer"
+
+# Le pido el nombre del jugador por teclado
+nombre_jugador = input("Por favor, ingresa tu nombre: ")
+
+# Imprimir un mensaje de bienvenida con el nombre
+print(f"Bienvenido, {nombre_jugador} ¡Prepárate para explorar jugar {laberinto_nombre}!")
+
+
+#Mi proyecto integrador primera parte Luis Villalobos
